3cda1142e6b5202a02c2daf351da6a8bdc566876
renangalterbrejao <renangalterbrejao@gmail.com>
2020-01-23 10:19:04 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -38,6 +38,12 @@ public class Program {
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New Id = " +newSeller.getId());
 		
+		System.out.println("\n=== TEST 5: Seller update ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Marta Waine");
+		sellerDao.update(seller);
+		System.out.println("Update completed");
+		
 	}
 
 }
