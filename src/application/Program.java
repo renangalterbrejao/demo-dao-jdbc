54520e1c0a42d37cac28cd036a329101969cf551
renangalterbrejao <renangalterbrejao@gmail.com>
2020-01-23 09:17:45 -0300

    findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -18,13 +18,21 @@ public class Program {
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
 		
-		System.out.println("\n=== TEST 1: Seller findByDepartment ===");
+		System.out.println("\n=== TEST 2: Seller findByDepartment ===");
 		Department department = new Department(2, null);
 		List<Seller> sellerList = sellerDao.findByDepartment(department);
-
+		
 		for(Seller obj : sellerList) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("\n=== TEST 3: Seller findAll ===");
+		sellerList = sellerDao.findAll();
+		
+		for(Seller obj : sellerList) {
+			System.out.println(obj);
+		}
+		
 	}
 
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -106,8 +106,51 @@ public class SellerDaoJDBC implements SellerDao {
 
 	@Override
 	public List<Seller> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		PreparedStatement st = null;
+		ResultSet rs = null;
+
+		try {
+
+			st = conn.prepareStatement(
+					"SELECT seller.*,department.Name as DepName "
+					+ "FROM seller INNER JOIN department "
+					+ "ON seller.DepartmentId = department.Id "
+					+ "ORDER BY Name");
+
+			rs = st.executeQuery();
+
+			List<Seller> sellerList = new ArrayList<>();
+			Map<Integer, Department> map = new HashMap<>();
+
+			while (rs.next()) {
+
+				Department dep = map.get(rs.getInt("departmentId"));
+
+				if (dep == null) {
+					dep = instantiateDepartment(rs);
+					map.put(rs.getInt("departmentId"), dep);
+				}
+
+				Seller seller = instantiateSeller(rs);
+				seller.setDepartment(dep);
+
+				sellerList.add(seller);
+			}
+
+			return sellerList;
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new DbException(e.getMessage());
+
+		} finally {
+
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+
+		}
+		
 	}
 
 	@Override
